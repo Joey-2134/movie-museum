c7162caa8be6b3cdabd95cfffd0172af578ede08
Joey-2134 <joeygalvin2134@gmail.com>
2024-10-25 10:46:33 +0100

    /Actors displaying data from backend

--- src/pages/Actors.tsx
+++ src/pages/Actors.tsx
@@ -1,7 +1,33 @@
+import {useQuery} from "react-query";
+import {ActorJSON} from "../types/types.ts";
+import {fetchActors} from "../api/actorRequests.ts";
+
 export default function Actors() {
+      const { isLoading, error, data } = useQuery<ActorJSON[], Error>(
+      'actors',
+      fetchActors
+     )
+    if (isLoading) return <div>Loading...</div>
+    if (error) return <div>Error: {error.message}</div>
+
+      // Check if data is empty
+      if (data && data.length === 0) {
+        return <div>No actors found</div>;
+      }
+
     return (
-        <div>
-            <h1>Actors</h1>
-        </div>
+        <>
+            {data?.map((actor: ActorJSON) => (
+             <div>
+               <ul>
+                 <li>
+                     <span>Name: {actor.firstName + " " + actor.lastName}</span>
+                     <br/>
+                     <span>Age: {actor.age}</span>
+                 </li>
+               </ul>
+             </div>
+         ))}
+       </>
     )
 }
