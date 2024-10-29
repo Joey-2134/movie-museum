f4dd1ec29c3c8ab0a468cf6136a70ce0d57b448c
Alex <axkviv24@gmail.com>
2024-10-29 08:53:00 +0000

    Genres.tsx and genreRequests.tsx created, testing TBD

---
+++ src/pages/Genres.tsx
@@ -0,0 +1,41 @@
+import {useQuery} from "react-query";
+import {GenreJSON} from "../types/types.ts";
+import {fetchGenres} from "../api/genreRequests.ts";
+
+import './Tables.css'
+
+export default function Genres() {
+      const { isLoading, error, data } = useQuery<GenreJSON[], Error>(
+      'actors',
+      fetchGenres
+     )
+    if (isLoading) return <div>Loading...</div>
+    if (error) return <div>Error: {error.message}</div>
+
+      // Check if data is empty
+      if (data && data.length === 0) {
+        return <div>No genres found</div>;
+      }
+
+    return (
+        <table className="tableContent">
+            <thead>
+            <tr>
+                <th className="header">Name</th>
+                {/* <th className="header">Age</th> */}
+            </tr>
+            </thead>
+            <tbody>
+                {data?.map((genre: GenreJSON) => (
+                    <tr>
+                        <td>{genre.name}</td>
+                    </tr>
+                ))}
+            </tbody>
+
+        </table>
+        //  <>
+       //
+       // </>
+    )
+}
